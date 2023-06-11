c83d9e80a617c0ac15c1892660f19a8692fadf97
Arshad Sheik <arshadsheik4801@gmail.com>
2023-06-11 11:58:15 +0530

    commit

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -52,12 +52,12 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price]');
+      elem.setAttribute('columns', '["top_ask_price"]');
       elem.setAttribute('aggregates', `
         {"stock":"distinct count",
           "top_ask_price":"avg",
           "top_bid_price":"avg",
-          "timestamp":"distinct count"},`);
+          "timestamp":"distinct count"}`);
     }
   }
 
